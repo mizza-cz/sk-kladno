a9c7160f1ab3810b8f86acc1db56030cf6dd9b53
mizza-cz <mizza.cz666@gmail.com>
2024-02-07 09:33:31 +0100

    match

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@
 //=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
 //=include ../../node_modules/masonry-layout/dist/masonry.pkgd.min.js
 //=include ../../node_modules/slick-carousel/slick/slick.js
+//=include ../../node_modules/fslightbox/index.js
 document.addEventListener("DOMContentLoaded", function () {
   //=include ../components/**/*.js
   //=include custom/*.js
