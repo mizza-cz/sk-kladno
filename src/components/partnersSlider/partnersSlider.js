7c1928eb6e7da25e5220c6452279ee9460f2961c
mizza-cz <mizza.cz666@gmail.com>
2024-03-08 13:52:39 +0100

    add fixes

--- src/components/partnersSlider/partnersSlider.js
+++ src/components/partnersSlider/partnersSlider.js
@@ -1,33 +1,33 @@
-$(".partnersSlider__inner").slick({
-  dots: false,
-  infinite: true,
-  arrows: true,
-  draggable: true,
+// $(".partnersSlider__inner").slick({
+//   dots: false,
+//   infinite: true,
+//   arrows: true,
+//   draggable: true,
 
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  speed: 2000,
-  autoplay: false,
-  prevArrow:
-    '<button class="slider__btn slider__btnprev"><img src="./images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
-  nextArrow:
-    '<button class="slider__btn slider__btnnext"><img src="./images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
-  responsive: [
-    {
-      breakpoint: 1401,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 4,
-      },
-    },
+//   slidesToShow: 5,
+//   slidesToScroll: 1,
+//   speed: 2000,
+//   autoplay: false,
+//   prevArrow:
+//     '<button class="slider__btn slider__btnprev"><img src="./images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
+//   nextArrow:
+//     '<button class="slider__btn slider__btnnext"><img src="./images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
+//   responsive: [
+//     {
+//       breakpoint: 1401,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 4,
+//       },
+//     },
 
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 2,
-        autoplay: true,
-        slidesToScroll: 2,
-      },
-    },
-  ],
-});
+//     {
+//       breakpoint: 900,
+//       settings: {
+//         slidesToShow: 2,
+//         autoplay: true,
+//         slidesToScroll: 2,
+//       },
+//     },
+//   ],
+// });
